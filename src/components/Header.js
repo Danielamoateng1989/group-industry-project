45ccda5d063afefc2e731159894ff743ee9d407a
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-06-25 07:09:38 -0400

    Add right navbar menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,32 @@ import {Container} from "react-bootstrap"
 const Header = () => {
 
     return (   
-  <div class="nav-right">
+  <>
+  <div class="nav-left">
   <Container>
-  <a class="active" href="/">START BUILDING TODAY</a>
-  <a href="/login">LOGIN</a>
+  <a class="active" href="/"><span>Self.</span></a>
+  <a href="/login">Learn</a>
+  <a href="/about">About</a>
+  <a href="/resources">Resources</a>
+  <a href="/reward">Get $10</a>
+  <a href="/help">Help</a>
+
+  
+ 
   </Container>
+  
+  
   </div>  
+  <Container>
+  <div class="nav-right">
+  <a href="/help">START BUILDING TODAY</a>
+  </div>
+  </Container>
+  </>
+
+  
+
+ 
 
 
 
