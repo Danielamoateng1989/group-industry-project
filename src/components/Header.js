346ed0dacb5dc721ffa91e7fa51e812c97359c8e
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-06-25 06:21:27 -0400

    Add top right LOGIN and START BUILDING TODAY menu items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,17 @@
-import { Navbar, Nav, Container} from "react-bootstrap"
+import {Container} from "react-bootstrap"
 
 const Header = () => {
 
-    return (
-
-        <header>
-        
-  <Navbar style={{backgroundColor: "#3BADEE"}} collapseOnSelect>
-      <Container>
-  <Navbar.Brand href="/">Self.</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto" >
-      <Nav.Link href="#">LEARN</Nav.Link>
-      <Nav.Link href="#">ABOUT</Nav.Link>
-      <Nav.Link href="#">RESOURCES</Nav.Link>
-      <Nav.Link href="#">GET $10</Nav.Link>
-      <Nav.Link href="#">HELP</Nav.Link>
-      <Nav.Link href="/calculator">RETIREMENT CALCULATOR</Nav.Link>
-
-    </Nav>
-  </Navbar.Collapse>
+    return (   
+  <div class="nav-right">
+  <Container>
+  <a class="active" href="/">START BUILDING TODAY</a>
+  <a href="/login">LOGIN</a>
   </Container>
-</Navbar>
-        </header>
-        
+  </div>  
+
+
+
     )
 }
 
