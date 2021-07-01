4743f316de4b7baa91aeedb924d1dda377f6e273
Andy Schachter <andy.schachter@gmail.com>
2021-07-01 14:30:37 -0400

    updates

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,29 +1,9 @@
 
 import { Bar } from "react-chartjs-2"
-<<<<<<< HEAD
-import calculator from '../utils/calculator'
-
-const Graph = () => {
-    const dummyData = {
-        startAge: 32, 
-        income: 75000, 
-        spouseIncome: 65000, 
-        amountSaved: 50000, 
-        retirementAge: 65, 
-        yearsRetired: 20, 
-        savingsRate: 20, 
-        inflation: 4, 
-        preRoi: 8, 
-        postRoi: 6, 
-        incomeRetirementPercent: 80, 
-        salaryIncrease: 3
-        }
-=======
 
 const Graph = (props) => {
     
         
->>>>>>> a38180fece4883d95a8722f8b8c12817b3f43122
     
     const formattedData = props.data
 
